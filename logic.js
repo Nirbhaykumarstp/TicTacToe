b1f9b14120ae6d41e80a7b3565fbebbf0063d992
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-08-11 11:12:17 +0530

    Validation for players name added

--- logic.js
+++ logic.js
@@ -4,37 +4,49 @@ let playerInfo = [
 ]
 
 
-function gameStart() {
+function gameStart() 
+{
     const plyr1Name = prompt("Enter the Player1 Name")
     const plyr2Name = prompt("Enter the Player2 Name")
+    if(!(plyr1Name===null || plyr2Name===null))
+    {
+        if(!(plyr1Name=="" || plyr2Name==""))
+        {
+            document.getElementById("Start").style.display="none"
+            document.getElementById("resetbtn").style.display="block"
+            playerInfo[0].p1Name = plyr1Name
+            playerInfo[0].isTurn = true
+            playerInfo[1].p2Name = plyr2Name
 
-    playerInfo[0].p1Name = plyr1Name
-    playerInfo[0].isTurn = true
-    playerInfo[1].p2Name = plyr2Name
+            console.log(plyr1Name, plyr2Name)
 
-    console.log(plyr1Name, plyr2Name)
-
-    document.getElementById("plyr-1-name").innerText = playerInfo[0].p1Name
-    document.getElementById("plyr-2-name").innerText = playerInfo[1].p2Name
-    let buttons = document.querySelectorAll(".btn")
-    console.log(buttons)
-
-    buttons.forEach((b) => {
-        b.addEventListener("click", () => {
-            if(playerInfo[0].isTurn)
-            {
-                b.innerText = "X"
-                playerInfo[0].isTurn = false
-                playerInfo[1].isTurn = true
-            }
-            else{
-                b.innerText = "O"
-                playerInfo[0].isTurn = true
-                playerInfo[1].isTurn = false
-            }
-        })
-    })
-    document.getElementById("turn").innerText = playerInfo[0].p1Name
-
+            document.getElementById("plyr-1-name").innerText = playerInfo[0].p1Name
+            document.getElementById("plyr-2-name").innerText = playerInfo[1].p2Name
+            let buttons = document.querySelectorAll(".btn")
+            console.log(buttons)
+            document.getElementById("turn").innerText = playerInfo[0].p1Name
+            buttons.forEach((b) => {
+                b.addEventListener("click", () => {
+                    if(playerInfo[0].isTurn)
+                    {
+                        b.innerText = "X"
+                        document.getElementById("turn").innerText = playerInfo[1].p2Name
+                        playerInfo[0].isTurn = false
+                        playerInfo[1].isTurn = true
+                    }
+                    else{
+                        b.innerText = "O"
+                        document.getElementById("turn").innerText = playerInfo[0].p1Name
+                        playerInfo[0].isTurn = true
+                        playerInfo[1].isTurn = false
+                    }
+                    b.disabled=true
+                })
+            })
+        }
+        else{
+            alert("Please Enter a appropriate Name")
+        }
+    }
 
 }
